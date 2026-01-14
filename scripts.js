f60a4ff5ac601755fae949e495903da5286a6da2
gamergo395 <4faragor@truroschool.com>
2026-01-14 15:51:06 +0000

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,25 @@
+function checkAccess() {
+    const code = document.getElementById('accessCode').value;
+    if (code.toUpperCase() === 'F0REVERB0RED') {
+        window.location.href = 'subpages/games.html';
+    } else if (code.toUpperCase() === 'TRUR0') {
+        window.location.href = 'subpages/mainpagest.html';   
+	} else if (code.toUpperCase() === 'AIGAME') {
+		window.location.href = 'subpages/aigames.html';
+	} else if (code.toUpperCase() === 'CHEESE') {
+		window.location.href = 'subpages/cheese.html';
+	} else if (code.toUpperCase() === 'HELLO') {
+    // Find the div where you want to show the message
+    const messageDiv = document.getElementById('messageDiv');
+
+    if (messageDiv) {
+        // Set the text content
+        messageDiv.textContent = 'YOU FOUND THE SECRET';
+        // Optionally style it
+        messageDiv.style.display = 'block';
+	}
+    } else {
+        alert('access denied: invalid code');
+    }
+    return false; // Prevent form submission
+    }
